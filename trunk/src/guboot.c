ec92ba4c49b2cf14b29cf8a5c8aa3a551170d771
patrik <patrik@69af5013-8a59-0410-84f7-ee1f1a1ceb62>
2008-10-22 23:05:47 +0000

    bkisofs integration

--- trunk/src/guboot.c
+++ trunk/src/guboot.c
@@ -22,6 +22,10 @@
 #include "guboot.h"
 #include "gui.h"
 
+/* need to include bk.h for access to bkisofs functions and structures */
+#include <stdio.h>
+#include "bk/bk.h"
+
 #include <stdlib.h>
 
 
@@ -109,9 +113,98 @@ gboolean guboot_modify_files (gchar *mntpoint)
 }
 
 
+/* you can use this to update a progress bar or something */
+void readProgressUpdaterCbk(VolInfo* volInfo)
+{
+    g_message ("Read progress updater");
+}
+
+
+void printNameAndContents(BkFileBase* base, int numSpaces)
+{
+    int count;
+    
+    /* print the spaces (indentation, for prettyness) */
+    for(count = 0; count < numSpaces; count++)
+        printf(" ");
+    
+    if(IS_DIR(base->posixFileMode))
+    {
+        /* print name of the directory */
+        printf("%s (directory)\n", base->name);
+        
+        /* print all the directory's children */
+        BkFileBase* child = BK_DIR_PTR(base)->children;
+        while(child != NULL)
+        {
+            printNameAndContents(child, numSpaces + 2);
+            child = child->next;
+        }
+    }
+    else if(IS_REG_FILE(base->posixFileMode))
+    {
+        /* print name and size of the file */
+        printf("%s (regular file), size %u\n", base->name, BK_FILE_PTR(base)->size);
+    }
+    else if(IS_SYMLINK(base->posixFileMode))
+    {
+        /* print name and target of the symbolic link */
+        printf("%s -> %s (symbolic link)\n", base->name, BK_SYMLINK_PTR(base)->target);
+    }
+}
+
+
 // Extrahiert eine ISO Datei ins angegebene Verzeichnis
 gboolean guboot_extract_iso (gchar *isofile, gchar *destination)
 {
+	VolInfo volInfo;
+	
+	gint rc;
+    
+    /* initialise volInfo, set it up to scan for duplicate files */
+    rc = bk_init_vol_info(&volInfo, true);
+    if(rc <= 0) {
+		g_warning (bk_get_error_string(rc));
+		return FALSE;
+	}
+	
+	/* open the iso file (supplied as argument 1) */
+    rc = bk_open_image(&volInfo, isofile);
+    if(rc <= 0) {
+		g_warning (bk_get_error_string(rc));
+		return FALSE;
+	}
+    
+    /* read information about the volume (required before reading directory tree) */
+    rc = bk_read_vol_info(&volInfo);
+    if(rc <= 0) {
+		g_warning (bk_get_error_string(rc));
+		return FALSE;
+	}
+	
+	/* read the directory tree */
+    if(volInfo.filenameTypes & FNTYPE_ROCKRIDGE)
+        rc = bk_read_dir_tree(&volInfo, FNTYPE_ROCKRIDGE, true, readProgressUpdaterCbk);
+    else if(volInfo.filenameTypes & FNTYPE_JOLIET)
+        rc = bk_read_dir_tree(&volInfo, FNTYPE_JOLIET, false, readProgressUpdaterCbk);
+    else
+        rc = bk_read_dir_tree(&volInfo, FNTYPE_9660, false, readProgressUpdaterCbk);
+    if(rc <= 0) {
+		//g_warning (bk_get_error_string(rc));
+		return FALSE;
+	}
+	
+	/* print the entire directory tree */
+    printNameAndContents (BK_BASE_PTR( &(volInfo.dirTree) ), 0);
+	
+	/* we're finished with this ISO, so clean up */
+    bk_destroy_vol_info(&volInfo);
+	
+	g_message ("Soweit alles ok!");
+	
+	//return FALSE;
+	
+	
 	gint ret;
 	gchar *command;
 	
